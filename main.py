c5f15a8b2d41844ef6b2cfd619ea6a397a347d4d
AnshulChamoli03 <anshulchamoli03@gmail.com>
2023-07-09 20:37:01 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,4 @@
+"""A imaginary company of real estate is there.
+till now they are using past data manually to predict price of real estate but this model is not very accurate.
+Now we have to develop a new model with higher accuracy"""
+import s
